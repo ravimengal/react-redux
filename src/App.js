10562960909c71451a3f360bfe658963bfc7c62f
ravimengal <mengal22ravi@gmail.com>
2021-09-19 04:01:14 -0700

    Creating action reducer combinereducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container card mt-5">
+      <h2>Simple Redux for increment and decrement </h2>
+      <button type="button" className="btn btn-success mt-4 mb-4">
+        increment
+      </button>
+
+      <div className="input-group  ">
+        <span className="input"></span>
+        <input type="text" />
+      </div>
+
+      <button type="button" className="btn btn-danger mt-4">
+        decrement
+      </button>
     </div>
   );
 }
